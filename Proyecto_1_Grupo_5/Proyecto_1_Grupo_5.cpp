a7c429574a6c19a97fc484e899de1bc92cb8d3e0
szorto <samuelzorto@gmail.com>
2023-09-18 19:50:52 -0600

    ya casi i rey

--- Proyecto_1_Grupo_5/Proyecto_1_Grupo_5.cpp
+++ Proyecto_1_Grupo_5/Proyecto_1_Grupo_5.cpp
@@ -61,22 +61,53 @@ string ajustarFecha(string s) {
 
 
 
-int main()
-{
+int main() {
 
 	vector<Alumno*> alumnos;
 	string semana;
 	string base = "Contenido del Curso";
 	string fecha = "2021-07-20";
-
+	int vueltas = 1;
 
 	for (int i = 1; i <= 5; i++) {
 		semana = "Semana 0" + to_string(i);
 		bool empezo = false;
-		while (i == 1) {
-			i++;
-			string directory = base + "/" + semana + "/" + fecha + ".txt";
-			fstream* file = new fstream(directory, ios::in);
+		switch (i) {
+		case 1:
+			fecha = "2021-07-20";
+			break;
+		case 2:
+			fecha = "2021-07-26";
+			break;
+		case 3:
+			fecha = "2021-08-02";
+			break;
+		case 4:
+			fecha = "2021-08-10";
+			break;
+		case 5:
+			fecha = "2021-07-16";
+			break;
+
+		}
+		vueltas = 1;
+		string directory = base + "/" + semana + "/" + fecha + ".txt";
+		fstream* file = new fstream(directory, ios::in);
+
+		std::experimental::filesystem::path fs = directory;
+		int count{ };
+		std::experimental::filesystem::path p1 = fs.parent_path();
+		for (auto& p : std::experimental::filesystem::directory_iterator(p1))
+		{
+			++count;
+		}
+
+		std::cout << "# of files in " << p1 << ": " << count << '\n';
+
+		while (vueltas <= count) {
+			vueltas++;
+			directory = base + "/" + semana + "/" + fecha + ".txt";
+			file = new fstream(directory, ios::in);
 
 			if (file->is_open()) cout << "Samuel sos un caregei\n";
 			if (!file) {
@@ -88,47 +119,119 @@ int main()
 				string nom = "";
 				bool audio = true;
 				cout << "*REVISANDO EL ARCHIVO:" << endl;
-				std::experimental::filesystem::path fs = directory;
-				cout << std::experimental::filesystem::absolute(fs) << "\n";
+				
+				cout << experimental::filesystem::absolute(fs) << "\n";
+				
 
+				cout << endl << "	";
 				file->seekg(0);
 
-				cout << "impresion\n";
+				//cout << "impresion\n";
+				string lineaVacia = "";
+				string fechaComparar = fecha + ":";
+				string nombre = ""; bool verificar = true; //si la variable verificar es true, se crea un alumno, a lo contrario, solo se actualiza;
+				size_t find; //usada para el String::find()
+
 
 				while (getline(*file, linea)) {
+					verificar = true;
+					if (linea == fechaComparar || linea.size() == 0) { cout << "I"; }
+					else {
+						//este for es para actualizar o crear un alumno en el vector
+						for (int j = 0; j < linea.size(); j++) {
+							if (linea.at(j) == ' ') {
+								//for (Alumno* alu : alumnos) {
+								//	if (alu->getNombre() == nombre) { //si encuentra el nombre en el vector, solo se va a actualizar
+								//		verificar = false;
+								//		if (linea.at(j + 1) == '-') {
+								//			alu->addAudio(false); 
+								//			cout << " si audio ";
+								//		}
+								//		else {
+								//			alu->addAudio(true); 
+								//			cout << " no audio ";
+								//		}
+								//		nombre = "";
+								//	}
+								//	
+								//}
+								
+								break;
+							}
+							else {
+								
+								nombre += linea.at(j);
+								if (nombre.size() == linea.size() || linea.at(j + 1) == ' ') {
+									for (Alumno* alu : alumnos) {
+										if (alu->getNombre() == nombre) {
+											verificar = false;
 
-
-					cout << linea << "\n";
-
-					/*if (((int)linea.at(0) >= 48 && (int)linea.at(0) <= 57) || linea.at(0) == ' ') {
-						cout << "I";
-					}
-					for (int i = 0; i < linea.length(); i++) {
-						if (linea.at(i) != ' ') {
-							nom += linea.at(i);
-						}
-						else {
-							cout << "[" + nom + "]";
-							for (int j = i; j < linea.length(); j++) {
-								if (linea.at(i) != '-') {
-									audio = false;
-									break;
+											find = linea.find("NO_AUDIO");
+											if (find != string::npos) {
+												alu->addAudio(true);
+											}
+											else {
+												alu->addAudio(false);
+											}
+											cout << "[" << nombre << "][A]FU, ";
+											nombre = "";
+											break;
+										}
+									}
 								}
-								audio = true;
+								
 							}
-							if (!encontrarAlumno(alumnos, nom)) {
-								Alumno* a = new Alumno(nom);
-								a->addAudio(audio);
-								alumnos.push_back(a);
-							}
-							break;
 						}
-					}*/
+						if (verificar) { //agrega el estudiante si no esta en el vector
+							cout << "[" << nombre << "][A] NC, ";
+							
+							Alumno* a = new Alumno(nombre);
+							nombre = "";
+							find = linea.find("NO_AUDIO");
+							if (find == string::npos) { a->addAudio(true); }
+							else { a->addAudio(false); }
+							
+							alumnos.push_back(a);
+						}
+
+						//cout << linea << endl;
+						
+
+
+
+						/*if (((int)linea.at(0) >= 48 && (int)linea.at(0) <= 57) || linea.at(0) == ' ') {
+							cout << "I";
+						}
+						for (int i = 0; i < linea.length(); i++) {
+							if (linea.at(i) != ' ') {
+								nom += linea.at(i);
+							}
+							else {
+								cout << "[" + nom + "]";
+								for (int j = i; j < linea.length(); j++) {
+									if (linea.at(i) != '-') {
+										audio = false;
+										break;
+									}
+									audio = true;
+								}
+								if (!encontrarAlumno(alumnos, nom)) {
+									Alumno* a = new Alumno(nom);
+									a->addAudio(audio);
+									alumnos.push_back(a);
+								}
+								break;
+							}
+						}*/
+					}
+					
 				}
+				cout << endl << endl;
+				cout << lineaVacia;
+				fecha = ajustarFecha(fecha);
+				file->close();
+				delete file;
 			}
-			ajustarFecha(fecha);
-			file->close();
-			delete file;
 		}
 	}
 }
